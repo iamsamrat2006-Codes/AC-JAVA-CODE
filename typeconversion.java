e2ecc0ab7433abb12c7fd528f1937f5b6ce7d813
iamsamrat2006-Codes <iamsamrat2006@gmail.com>
2026-08-29 20:13:54 +0530

    update

---
+++ typeconversion.java
@@ -0,0 +1,15 @@
+//CONVERSION HAPPENS WHEN WE ASSIGN A VALUE OF ONE DATA TYPE TO ANOTHER DATA TYPE.
+//= TYPE COMPATIBLE DATA TYPES // DESTINATION TYPE>> SOURCE TYPE
+
+
+
+
+public class typeconversion {
+  public static void main(String args[]) {
+    int a = 10;
+    float b = a; // implicit type conversion
+    System.out.println(b);
+  } //output: 10.0
+
+  
+}
