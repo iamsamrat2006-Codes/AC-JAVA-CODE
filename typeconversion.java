1a9079884a535ed2886d0fd8a66f3ad48478e1ac
iamsamrat2006-Codes <iamsamrat2006@gmail.com>
2026-08-29 20:21:04 +0530

    update

--- typeconversion.java
+++ typeconversion.java
@@ -1,6 +1,11 @@
 //CONVERSION HAPPENS WHEN WE ASSIGN A VALUE OF ONE DATA TYPE TO ANOTHER DATA TYPE.
-//= TYPE COMPATIBLE DATA TYPES // DESTINATION TYPE>> SOURCE TYPE
-
+//= TYPE COMPATIBLE DATA TYPES // DESTINATION TYPE size eg.long>> SOURCE TYPE size eg.int
+//long conversion of <int to long > is possible but not vice versa because long is bigger than int. [long>>int]
+//===implicit type conversion (typecasting) is done by the compiler automatically when we assign a value of one data type to another data type.
+//example: int a = 10; float b = a; // implicit type conversion
+//===widening conversion: when we assign a value of smaller data type to a bigger data type. eg. int to long, float to double, char to int etc.
+//===explicit type conversion (typecasting) is done by the programmer when we assign a value of one data type to another data type.
+//example: int a = 10; float b = (float)a; // explicit type conversion
 
 
 
